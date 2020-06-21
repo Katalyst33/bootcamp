139945943c7018b3451a71a08209d7554dc29768
hibrid <samuelnmeje@gmail.com>
2020-06-21 19:04:19 +0400

    updated mongo uri

--- config/db.js
+++ config/db.js
@@ -16,16 +16,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-/*
-mongoose.connect(  "mongodb://127.0.0.1/newagebot",
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    }).then(() => {
-    console.log("Connected to Mongo Database Successfully");
-}).catch((err) => {
-    console.log("Failed to Connect to MongoDb ", err);
-});
-*/
