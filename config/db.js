48db7974967cae0f17c97725b66d62592ca2f8fe
hibrid <samuelnmeje@gmail.com>
2020-06-21 18:50:42 +0400

    updated mongo uri

--- config/db.js
+++ config/db.js
@@ -1,7 +1,12 @@
+const dotenv = require("dotenv");
+
+dotenv.config({
+  path: "./config.env",
+});
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-  const conn = await mongoose.connect("mongodb://127.0.0.1/devcamper", {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
